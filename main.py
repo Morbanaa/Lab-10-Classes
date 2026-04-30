25ce493383634bb4825e4c79b26b21ae5c9d5c34
Morbanaa <theodorerodd@gmail.com>
2026-04-30 12:38:34 -0500

    Created info class

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-ECHO is on.
+# Teddy Rodd
+# Lab 10 
+#Personal Information Class
+
+class PersonalInfo():
+    def __init__(self,name,address,age,phone_number):
+        self.name = name
+        self.address = address
+        self.age = age
+        self.phone_number = phone_number
+
+    def display(self):
+        print("-------------------------")
+        print("           Data:")
+        print("-------------------------")
+        print(f"Name: {self.name}")
+        print(f"Address: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Phone: {self.phone_number}")
+
+
