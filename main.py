36d2a8fc8276563e86382492149bd8df988ee314
Morbanaa <theodorerodd@gmail.com>
2026-04-30 14:32:17 -0500

    Filled out collect info func. Takes user input and uses it to create objects and call their display methods

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class PersonalInfo():
         print("           Data:")
         print("-------------------------")
         print(f"Name: {self.name}")
-        print(f"Address: {self.name}")
+        print(f"Address: {self.address}")
         print(f"Age: {self.age}")
         print(f"Phone: {self.phone_number}")
 
@@ -23,10 +23,23 @@ def main():
     collect_info()
 
 
-def collect_info():
-    pass
 
+def collect_info():  
+    your_name, your_address, your_age, your_phone_number = input("Enter your name, address, age, and phone number with a space between each: ").split()
 
+    friend_name, friend_address, friend_age, friend_phone_number = input("Enter a friends name, address, age, and phone number with a space between each: ").split()
 
-if __name__ == "__main":
+    family_name, family_address, family_age, family_phone_number = input("Enter a family members name, address, age, and phone number with a space between each: ").split()
+
+    your_info = PersonalInfo(your_name,your_address,your_age,your_phone_number)
+    friend_info = PersonalInfo(friend_name,friend_address,friend_age,friend_phone_number)
+    family_info = PersonalInfo(family_name,family_address,family_age,family_phone_number)
+
+    your_info.display()
+    friend_info.display()
+    family_info.display()
+
+   
+
+if __name__ == "__main__":
     main()
